1e902588ce89c20d99745a038c0b6145e8ca84a8
Mehdi Nassim KHODJA <khodjamehdinassim@gmail.com>
2021-11-06 01:37:07 +0100

    fix: add class-transformer for explicitConversion (String to Number)

--- src/cryptos/cryptos.service.ts
+++ src/cryptos/cryptos.service.ts
@@ -5,8 +5,9 @@ import {
   ServiceUnavailableException,
 } from '@nestjs/common';
 import { CoinCapApiService } from './coincapapi.service';
-import { Crypto } from './entities/crypto.entity';
+import { Crypto, CryptoCurrency } from './entities/crypto.entity';
 import { Cache } from 'cache-manager';
+import { plainToClass } from 'class-transformer';
 
 @Injectable()
 export class CryptosService {
@@ -25,8 +26,12 @@ export class CryptosService {
     // if not found in cache, fetching from API
     if (cryptos == null) {
       try {
+        // fetching
         const response = await this.coinCapApiService.getAssets();
-        cryptos = response.data['data'];
+        // transforming
+        cryptos = response.data['data'].map((v) =>
+          plainToClass(CryptoCurrency, v, { enableImplicitConversion: true }),
+        );
         // adding an iconUrl to each crypto
         if (cryptos && cryptos.length) {
           cryptos = cryptos.map((v) => ({
@@ -39,7 +44,7 @@ export class CryptosService {
       } catch (err) {
         throw new ServiceUnavailableException(err.status, err.message);
       }
-      // caching the result
+      // caching the result for 10s
       await this.cacheManager.set(this.assetsCacheKey, cryptos, { ttl: 10 });
     }
     return cryptos;
--- src/cryptos/entities/crypto.entity.ts
+++ src/cryptos/entities/crypto.entity.ts
@@ -1,3 +1,11 @@
+import {
+  IsInt,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsUrl,
+} from 'class-validator';
+
 export interface Crypto {
   id: string;
   rank: number;
@@ -11,5 +19,46 @@ export interface Crypto {
   changePercent24Hr: number;
   vwap24Hr: number;
   explorer: string;
+  iconUrl?: string;
+}
+
+export class CryptoCurrency implements Crypto {
+  @IsNotEmpty()
+  id: string;
+
+  @IsInt()
+  rank: number;
+
+  @IsNotEmpty()
+  symbol: string;
+
+  @IsNotEmpty()
+  name: string;
+
+  @IsNumber()
+  supply: number;
+
+  @IsNumber()
+  maxSupply: number;
+
+  @IsNumber()
+  marketCapUsd: number;
+
+  @IsNumber()
+  volumeUsd24Hr: number;
+
+  @IsNumber()
+  priceUsd: number;
+
+  @IsNumber()
+  changePercent24Hr: number;
+
+  @IsNumber()
+  vwap24Hr: number;
+
+  @IsUrl()
+  explorer: string;
+
+  @IsOptional()
   iconUrl?: string;
 }
