25ee5901d6be86cc5df03148502c158c3c6e253c
Mehdi Nassim KHODJA <khodjamehdinassim@gmail.com>
2021-11-07 20:42:56 +0100

    fix

--- backend/src/app.controller.spec.ts
+++ backend/src/app.controller.spec.ts
@@ -1,12 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CryptosModule } from './cryptos/cryptos.module';
+import { ScheduleModule } from '@nestjs/schedule';
 
 describe('AppController', () => {
   let appController: AppController;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
+      imports: [CryptosModule, ScheduleModule.forRoot()],
       controllers: [AppController],
       providers: [AppService],
     }).compile();
